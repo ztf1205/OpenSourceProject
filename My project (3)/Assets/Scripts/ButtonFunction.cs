d26c794a112f3f7ffe316f293262589a46d0cce9
Yohan <ztf1205@naver.com>
2022-03-26 16:33:35 +0900

    로비 추가 및 gitignore수정
    타이틀과 시작버튼이 있는 로비 추가
    유니티 임시 파일을 커밋하지 않도록 gitignore수정

---
+++ My project (3)/Assets/Scripts/ButtonFunction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunction : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    
+
+}
