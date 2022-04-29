274ff7d28f3d8c3bd90eed11b5ed989fd2a2898d
DESKTOP-B36N7MH\USER <yunho081544@gmail.com>
2022-04-29 16:51:43 +0900

    텔레포트 기능 구현
    텔레포트 기능 완성

--- My project (3)/Assets/Scripts/Game/Object/Teleportation.cs
+++ My project (3)/Assets/Scripts/Game/Object/Teleportation.cs
@@ -6,15 +6,20 @@ public class Teleportation : MonoBehaviour
 {
     public GameObject Portal;
     public GameObject Player;
+    private int P_Flag = 0;
     bool Pressed;
 
     //윗 방향키 입력 시 (아직 구현이 안됨)
     private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Pressed = true;
-            Debug.Log("press");
+            //윗방향키나 w를 입력시 순간이동
+            if (P_Flag == 1)
+            {
+                //다른 포탈 위치로 순간이동
+                StartCoroutine(Teleport());
+            }
         }
     }
 
@@ -23,20 +28,23 @@ public class Teleportation : MonoBehaviour
         //충돌자가 플레이어이면
         if (collision.gameObject.tag == "Player")
         {
-            //윗방향키나 w를 입력시 순간이동
-            //if (Pressed)
-           // {
-           //다른 포탈 위치로 순간이동
-                  Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
-           // }
-            // StartCoroutine(Teleport());
+
+            P_Flag = 1;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            P_Flag = 0;
         }
     }
 
     // 이 함수를 이용하면 순간이동에 딜레이를 줄 수 있음
-    //IEnumerator Teleport()
-    // {
-    //     yield return new WaitForSeconds(0);
-    //     Player.transform.position = new Vector2(Portal.transform.position.x + 1, Portal.transform.position.y);
-    // }
+    IEnumerator Teleport()
+     {
+        yield return new WaitForSeconds(0.2f);
+        Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+    }
 }
