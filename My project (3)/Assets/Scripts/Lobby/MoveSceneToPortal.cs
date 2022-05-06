06e69feed21a44c580e698c502ce1b41175d57e6
DESKTOP-9BF7S1U\Kang Mingu <rkdalsrn222@naver.com>
2022-05-06 13:37:11 +0900

    MoveSceneToPortal 추가

---
+++ My project (3)/Assets/Scripts/Lobby/MoveSceneToPortal.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveSceneToPortal : MonoBehaviour
+{
+    public string nextScene;
+    public GameObject Player;
+    private int Flag = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // 윗 방향키 입력시 Scene 이동
+            if(Flag == 1)
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //충돌자가 플레이어이면
+        if (collision.gameObject.tag == "Player")
+        {
+            Flag = 1;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Flag = 0;
+        }
+    }
+}
