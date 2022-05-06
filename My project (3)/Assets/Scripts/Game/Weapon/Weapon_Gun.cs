d1d0c62fe4ea21da88f8372572a55b13a8b68da0
Yohan <ztf1205@naver.com>
2022-05-06 20:56:03 +0900

    투사체 크기 증가 업그레이드 적용
    Weapon_Gun에서 공격할 때 투사체 크기 업그레이드 적용하도록 변경

--- My project (3)/Assets/Scripts/Game/Weapon/Weapon_Gun.cs
+++ My project (3)/Assets/Scripts/Game/Weapon/Weapon_Gun.cs
@@ -32,19 +32,29 @@ public class Weapon_Gun : MonoBehaviour
             //시간초기화
             timer = 0f;
 
-            //불릿 생성, 위치 할당, 회전
-            GameObject bullet = Instantiate(attack_Bullet);
-            bullet.transform.position = this.transform.position;
-
-            //불릿 마우스 방향으로 회전
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            bullet.transform.rotation = Quaternion.Euler(0f, 0f,
-                Mathf.Atan2(mousePos.x - this.transform.position.x, mousePos.y - this.transform.position.y) * Mathf.Rad2Deg * -1 + 90);
-
-            //효과음 재생
-            audioSource.Play();
+            Attack();
 
         }
 
     }
+
+    private void Attack()
+    {
+        //불릿 생성, 위치 할당, 회전
+        GameObject bullet = Instantiate(attack_Bullet);
+        bullet.transform.position = this.transform.position;
+
+        //불릿 마우스 방향으로 회전
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        bullet.transform.rotation = Quaternion.Euler(0f, 0f,
+            Mathf.Atan2(mousePos.x - this.transform.position.x, mousePos.y - this.transform.position.y) * Mathf.Rad2Deg * -1 + 90);
+
+        bullet.transform.localScale=new Vector3(bullet.transform.localScale.x+ bullet.transform.localScale.x * DataController.GetGameValue(Constants.PROJECTILESIZE_IDX)
+            , bullet.transform.localScale.y + bullet.transform.localScale.y * DataController.GetGameValue(Constants.PROJECTILESIZE_IDX)
+            , bullet.transform.localScale.z);
+
+        //효과음 재생
+        audioSource.Play();
+    }
+
 }
