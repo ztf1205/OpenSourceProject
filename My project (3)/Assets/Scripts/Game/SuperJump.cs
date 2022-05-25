28defc1400a9f2930dc14896220f511c2f1e780e
DESKTOP-B36N7MH\USER <yunho081544@gmail.com>
2022-05-26 00:18:46 +0900

    효과음 적용
    업그레이드, 스프링 밟을 시 효과음 적용
    상자 획득시 효과음 경우 오류 있음

--- My project (3)/Assets/Scripts/Game/SuperJump.cs
+++ My project (3)/Assets/Scripts/Game/SuperJump.cs
@@ -6,12 +6,22 @@ public class SuperJump : MonoBehaviour
 {
     [SerializeField]
     private float bounce_Force = 20f;
-    
+
+    AudioSource audioSource;
+    public AudioClip StepOnSound;
+
+    private void Awake()
+    {
+
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce_Force, ForceMode2D.Impulse);
+            this.audioSource.Play();
         }
     }
 }
